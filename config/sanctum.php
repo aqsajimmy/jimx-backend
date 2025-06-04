f1091851281a093002abbc3a7cb32ae5068fa5f7
aqsajimmy <aqsa.jimmy24@gmail.com>
2025-06-04 18:34:06 +0700

    fix:cors

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,8 +17,9 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,localhost:5173',
         Sanctum::currentApplicationUrlWithPort(),
+        env('FRONTED_URL') ? ',' . parse_url(env('FRONTED_URL'), PHP_URL_HOST) : '',
         // Sanctum::currentRequestHost(),
     ))),
 
